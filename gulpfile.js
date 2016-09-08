c2340786b931bf12fcc1789fbd91b6b1e31d75be
Dietrich <dietrich.kriworotow@gmail.com>
2016-09-08 09:24:26 +0000

    created gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@ gulp.task("default", function() {
     console.log("Hello Gulp");
 });
 
-gulp.task("browser-sync", function( {
+gulp.task("browser-sync", function(){
     browserSync({
         files: "index.html, css/style.css, js/main.js/main",
         port: 8082  
     });
   
-}));
+});
